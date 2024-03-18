bdf1ae0191a83868ddd5ca01b46902f8baa39cfd
TheMadDrunk <iophamza2@gmail.com>
2024-03-18 06:17:34 +0100

    fetching events

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,68 @@
-import * as React from 'react';
-import Map, {Source, Layer} from 'react-map-gl/maplibre';
-import type {CircleLayer} from 'react-map-gl/maplibre';
-import type {FeatureCollection} from 'geojson';
+import { useEffect, useState } from "react";
+import {supabaseClient } from "./createClient"
 
-const geojson: FeatureCollection = {
-  type: 'FeatureCollection',
-  features: [
-    {type: 'Feature', geometry: {type: 'Point', coordinates: [-122.4, 37.8]}}
-  ]
+
+export default function App() {
+
+	const [events,setEvents] = useState([]);
+	
+	async function fetchEvents() {
+		const {data} = await supabaseClient.from('events').select('*');
+		setEvents(data);
+	}
+
+	useEffect(()=>{
+		fetchEvents();
+	},[])
+
+	return (
+		<div className="container max-w-2xl mx-auto">
+			
+			<div className=" ">
+				<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+					<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+						<tr>
+							<th scope="col" className="px-6 py-3">
+								Name
+							</th>
+							<th scope="col" className="px-6 py-3">
+								Venue
+							</th>
+							<th scope="col" className="px-6 py-3">
+								Date
+							</th>
+							<th scope="col" className="px-6 py-3">
+								Action
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							events.map((ev,index)=>{
+								return (
+									<tr key={index} className="bg-white dark:bg-gray-800">
+							<th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+								{ev.name}
+							</th>
+							<td className="px-6 py-4">
+								{ev.venue}
+							</td>
+							<td className="px-6 py-4">
+								{ev.date}
+							</td>
+							<td className="px-6 py-4">
+								<button type="button" className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:focus:ring-yellow-900">View</button>
+							</td>
+						</tr>
+								)
+							})
+						}
+						
+					</tbody>
+				</table>
+			</div>
+			<div /></div>
+	);
+
 };
 
-const layerStyle: CircleLayer = {
-  id: 'point',
-  type: 'circle',
-  paint: {
-    'circle-radius': 10,
-    'circle-color': '#007cbf'
-  }
-};
-
-function App() {
-  return <Map
-   dragPan={false} 
-   maxZoom={9}
-  minZoom={7}
-  initialViewState={{
-    longitude: -4.671,
-    latitude: 32.6,
-  }}
-  style={{width: 600, height: 400}}
-    mapStyle="/public/osm_liberty.json"
-  >
-    <Source id="my-data" type="geojson" data={geojson}>
-      <Layer {...layerStyle} />
-    </Source>
-  </Map>;
-}
-
-export default App
