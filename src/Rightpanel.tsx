8997d2f1bd3da6998831b8830f7b784f00fc1204
TheMadDrunk <uhamza.alaoui@gmail.com>
2024-03-18 22:18:26 +0000

    fe

---
+++ src/Rightpanel.tsx
@@ -0,0 +1,12 @@
+
+export default function Rightpanel({data}) {
+
+    return (
+        <div className=" absolute z-10 right-10 top-10 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            {!data && <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Click where you want to sit</h5>
+            </a>}
+            
+        </div>
+    )
+}
